7493f2efea720c5192e69b04c62ee4b83827121b
xijiangping <xijiangping@jd.com>
2017-10-31 13:19:37 +0800

    mysql to excel

---
+++ src/main/java/com/jd/databaseDocer/Main.java
@@ -0,0 +1,14 @@
+package com.jd.databaseDocer;
+
+import com.jd.databaseDocer.View.MainFrame;
+
+/**
+ * @author xijiangping
+ * @date 2017/10/31
+ * @use .
+ */
+public class Main {
+	public static void main(String[] args) {
+		MainFrame.starup();
+	}
+}
